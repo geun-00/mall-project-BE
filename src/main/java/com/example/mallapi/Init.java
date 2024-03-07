cfb37fcb54da2d1b055fbd064d51df3525e0eb4b
장근영 <jky00914@naver.com>
2024-03-07 16:37:40 +0900

    Init 데이터

---
+++ src/main/java/com/example/mallapi/Init.java
@@ -0,0 +1,47 @@
+package com.example.mallapi;
+
+import com.example.mallapi.domain.Todo;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.Random;
+
+@Component
+@RequiredArgsConstructor
+public class Init {
+
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.init();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+        private final Random random = new Random();
+        public void init() {
+
+            for (int i = 1; i <= 500; i++) {
+                Todo todo = Todo.builder()
+                                .title("TITLE - " + i)
+                                .content("CONTENT - " + i)
+                                .complete(i % 2 == 0)
+                                .dueDate(generateRandomDate())
+                                .build();
+                em.persist(todo);
+            }
+        }
+        private LocalDate generateRandomDate() {
+            // 최대 30일까지 랜덤으로 더함
+            return LocalDate.now().plusDays(random.nextInt(30));
+        }
+    }
+}
